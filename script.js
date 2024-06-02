cfeab077f3b02c5deea796940ed9f67192c01792
Kenny <double.brain560@gmail.com>
2024-06-03 05:16:08 +0900

    delete collision

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.getElementById('menuButton').addEventListener('click', function() {
     var menu = document.getElementById('menu');
     if (menu.classList.contains('hidden')) {
@@ -7,13 +6,3 @@ document.getElementById('menuButton').addEventListener('click', function() {
         menu.classList.add('hidden');
     }
 });
-=======
-document.getElementById('menuButton').addEventListener('click', function() {
-    var menu = document.getElementById('menu');
-    if (menu.classList.contains('hidden')) {
-        menu.classList.remove('hidden');
-    } else {
-        menu.classList.add('hidden');
-    }
-});
->>>>>>> 1b790a2be80233dc84064c4bb7b78f90fed96247
